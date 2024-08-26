a61a50d7617214db8a7692337230d2743c5aab38
jonathan <jfuentes@rvohealth.com>
2024-08-26 16:29:07 -0600

    update: themes

--- Sources/Theming/Theme.swift
+++ Sources/Theming/Theme.swift
@@ -14,26 +14,12 @@ public protocol Theme {
     var buttonBackgroundColor: UIColor { get }
     var buttonTextColor: UIColor { get }
     var font: UIFont { get }
-}
-
-public struct Q4LTheme: Theme {
-    public var backgroundColor: UIColor { .white }
-    public var textColor: UIColor { .black }
-    public var buttonBackgroundColor: UIColor { .systemBlue }
-    public var buttonTextColor: UIColor { .white }
-    public var font: UIFont { .systemFont(ofSize: 16) }
-
-    public init() {}
-}
-
-public struct RallyTheme: Theme {
-    public var backgroundColor: UIColor { .black }
-    public var textColor: UIColor { .white }
-    public var buttonBackgroundColor: UIColor { .darkGray }
-    public var buttonTextColor: UIColor { .white }
-    public var font: UIFont { .systemFont(ofSize: 16) }
-
-    public init() {}
+    var buttonFont: UIFont { get }
+    var titleFont: UIFont { get }
+    var paragraphFont: UIFont { get }
+    var buttonSize: CGSize { get }
+    var titleSize: CGSize { get }
+    var paragraphSize: CGSize { get }
 }
 
 public struct DarkModeTheme: Theme {
@@ -41,8 +27,14 @@ public struct DarkModeTheme: Theme {
     public var textColor: UIColor { .lightGray }
     public var buttonBackgroundColor: UIColor { .gray }
     public var buttonTextColor: UIColor { .white }
-    public var font: UIFont { .systemFont(ofSize: 18, weight: .bold) }
-
+    public var font: UIFont { .systemFont(ofSize: 18, weight: .regular) }
+    public var buttonFont: UIFont { .systemFont(ofSize: 16, weight: .medium) }
+    public var titleFont: UIFont { .systemFont(ofSize: 24, weight: .bold) }
+    public var paragraphFont: UIFont { .systemFont(ofSize: 16, weight: .regular) }
+    public var buttonSize: CGSize { CGSize(width: 120, height: 44) }
+    public var titleSize: CGSize { CGSize(width: 200, height: 50) }
+    public var paragraphSize: CGSize { CGSize(width: 300, height: 200) }
+    
     public init() {}
 }
 
@@ -52,6 +44,158 @@ public struct LightModeTheme: Theme {
     public var buttonBackgroundColor: UIColor { .lightGray }
     public var buttonTextColor: UIColor { .black }
     public var font: UIFont { .systemFont(ofSize: 18, weight: .regular) }
+    public var buttonFont: UIFont { .systemFont(ofSize: 16, weight: .medium) }
+    public var titleFont: UIFont { .systemFont(ofSize: 24, weight: .bold) }
+    public var paragraphFont: UIFont { .systemFont(ofSize: 16, weight: .regular) }
+    public var buttonSize: CGSize { CGSize(width: 120, height: 44) }
+    public var titleSize: CGSize { CGSize(width: 200, height: 50) }
+    public var paragraphSize: CGSize { CGSize(width: 300, height: 200) }
+    
+    public init() {}
+}
 
+// Define the themes inspired by popular apps
+
+public struct FacebookTheme: Theme {
+    public var backgroundColor: UIColor { .systemBlue }
+    public var textColor: UIColor { .white }
+    public var buttonBackgroundColor: UIColor { .darkGray }
+    public var buttonTextColor: UIColor { .white }
+    public var font: UIFont { .systemFont(ofSize: 16) }
+    public var buttonFont: UIFont { .systemFont(ofSize: 14, weight: .medium) }
+    public var titleFont: UIFont { .systemFont(ofSize: 22, weight: .bold) }
+    public var paragraphFont: UIFont { .systemFont(ofSize: 16, weight: .regular) }
+    public var buttonSize: CGSize { CGSize(width: 130, height: 50) }
+    public var titleSize: CGSize { CGSize(width: 200, height: 50) }
+    public var paragraphSize: CGSize { CGSize(width: 300, height: 150) }
+    
+    public init() {}
+}
+
+public struct InstagramTheme: Theme {
+    public var backgroundColor: UIColor { .systemPurple }
+    public var textColor: UIColor { .white }
+    public var buttonBackgroundColor: UIColor { .darkGray }
+    public var buttonTextColor: UIColor { .white }
+    public var font: UIFont { .systemFont(ofSize: 16) }
+    public var buttonFont: UIFont { .systemFont(ofSize: 14, weight: .medium) }
+    public var titleFont: UIFont { .systemFont(ofSize: 22, weight: .bold) }
+    public var paragraphFont: UIFont { .systemFont(ofSize: 16, weight: .regular) }
+    public var buttonSize: CGSize { CGSize(width: 130, height: 50) }
+    public var titleSize: CGSize { CGSize(width: 200, height: 50) }
+    public var paragraphSize: CGSize { CGSize(width: 300, height: 150) }
+    
+    public init() {}
+}
+
+public struct TwitterTheme: Theme {
+    public var backgroundColor: UIColor { .systemBlue }
+    public var textColor: UIColor { .white }
+    public var buttonBackgroundColor: UIColor { .darkGray }
+    public var buttonTextColor: UIColor { .white }
+    public var font: UIFont { .systemFont(ofSize: 16) }
+    public var buttonFont: UIFont { .systemFont(ofSize: 14, weight: .medium) }
+    public var titleFont: UIFont { .systemFont(ofSize: 22, weight: .bold) }
+    public var paragraphFont: UIFont { .systemFont(ofSize: 16, weight: .regular) }
+    public var buttonSize: CGSize { CGSize(width: 130, height: 50) }
+    public var titleSize: CGSize { CGSize(width: 200, height: 50) }
+    public var paragraphSize: CGSize { CGSize(width: 300, height: 150) }
+    
+    public init() {}
+}
+
+public struct LinkedInTheme: Theme {
+    public var backgroundColor: UIColor { .systemBlue }
+    public var textColor: UIColor { .white }
+    public var buttonBackgroundColor: UIColor { .darkGray }
+    public var buttonTextColor: UIColor { .white }
+    public var font: UIFont { .systemFont(ofSize: 16) }
+    public var buttonFont: UIFont { .systemFont(ofSize: 14, weight: .medium) }
+    public var titleFont: UIFont { .systemFont(ofSize: 22, weight: .bold) }
+    public var paragraphFont: UIFont { .systemFont(ofSize: 16, weight: .regular) }
+    public var buttonSize: CGSize { CGSize(width: 130, height: 50) }
+    public var titleSize: CGSize { CGSize(width: 200, height: 50) }
+    public var paragraphSize: CGSize { CGSize(width: 300, height: 150) }
+    
+    public init() {}
+}
+
+public struct WhatsAppTheme: Theme {
+    public var backgroundColor: UIColor { .systemGreen }
+    public var textColor: UIColor { .white }
+    public var buttonBackgroundColor: UIColor { .darkGray }
+    public var buttonTextColor: UIColor { .white }
+    public var font: UIFont { .systemFont(ofSize: 16) }
+    public var buttonFont: UIFont { .systemFont(ofSize: 14, weight: .medium) }
+    public var titleFont: UIFont { .systemFont(ofSize: 22, weight: .bold) }
+    public var paragraphFont: UIFont { .systemFont(ofSize: 16, weight: .regular) }
+    public var buttonSize: CGSize { CGSize(width: 130, height: 50) }
+    public var titleSize: CGSize { CGSize(width: 200, height: 50) }
+    public var paragraphSize: CGSize { CGSize(width: 300, height: 150) }
+    
+    public init() {}
+}
+
+public struct SlackTheme: Theme {
+    public var backgroundColor: UIColor { .systemGray }
+    public var textColor: UIColor { .white }
+    public var buttonBackgroundColor: UIColor { .darkGray }
+    public var buttonTextColor: UIColor { .white }
+    public var font: UIFont { .systemFont(ofSize: 16) }
+    public var buttonFont: UIFont { .systemFont(ofSize: 14, weight: .medium) }
+    public var titleFont: UIFont { .systemFont(ofSize: 22, weight: .bold) }
+    public var paragraphFont: UIFont { .systemFont(ofSize: 16, weight: .regular) }
+    public var buttonSize: CGSize { CGSize(width: 130, height: 50) }
+    public var titleSize: CGSize { CGSize(width: 200, height: 50) }
+    public var paragraphSize: CGSize { CGSize(width: 300, height: 150) }
+    
+    public init() {}
+}
+
+public struct SpotifyTheme: Theme {
+    public var backgroundColor: UIColor { .systemGreen }
+    public var textColor: UIColor { .white }
+    public var buttonBackgroundColor: UIColor { .darkGray }
+    public var buttonTextColor: UIColor { .white }
+    public var font: UIFont { .systemFont(ofSize: 16) }
+    public var buttonFont: UIFont { .systemFont(ofSize: 14, weight: .medium) }
+    public var titleFont: UIFont { .systemFont(ofSize: 22, weight: .bold) }
+    public var paragraphFont: UIFont { .systemFont(ofSize: 16, weight: .regular) }
+    public var buttonSize: CGSize { CGSize(width: 130, height: 50) }
+    public var titleSize: CGSize { CGSize(width: 200, height: 50) }
+    public var paragraphSize: CGSize { CGSize(width: 300, height: 150) }
+    
+    public init() {}
+}
+
+public struct UberTheme: Theme {
+    public var backgroundColor: UIColor { .black }
+    public var textColor: UIColor { .white }
+    public var buttonBackgroundColor: UIColor { .systemRed }
+    public var buttonTextColor: UIColor { .white }
+    public var font: UIFont { .systemFont(ofSize: 16) }
+    public var buttonFont: UIFont { .systemFont(ofSize: 14, weight: .medium) }
+    public var titleFont: UIFont { .systemFont(ofSize: 22, weight: .bold) }
+    public var paragraphFont: UIFont { .systemFont(ofSize: 16, weight: .regular) }
+    public var buttonSize: CGSize { CGSize(width: 130, height: 50) }
+    public var titleSize: CGSize { CGSize(width: 200, height: 50) }
+    public var paragraphSize: CGSize { CGSize(width: 300, height: 150) }
+    
+    public init() {}
+}
+
+public struct GoogleTheme: Theme {
+    public var backgroundColor: UIColor { .white }
+    public var textColor: UIColor { .black }
+    public var buttonBackgroundColor: UIColor { .systemBlue }
+    public var buttonTextColor: UIColor { .white }
+    public var font: UIFont { .systemFont(ofSize: 16) }
+    public var buttonFont: UIFont { .systemFont(ofSize: 14, weight: .medium) }
+    public var titleFont: UIFont { .systemFont(ofSize: 22, weight: .bold) }
+    public var paragraphFont: UIFont { .systemFont(ofSize: 16, weight: .regular) }
+    public var buttonSize: CGSize { CGSize(width: 130, height: 50) }
+    public var titleSize: CGSize { CGSize(width: 200, height: 50) }
+    public var paragraphSize: CGSize { CGSize(width: 300, height: 150) }
+    
     public init() {}
 }
--- Sources/Theming/ThemeManager.swift
+++ Sources/Theming/ThemeManager.swift
@@ -6,36 +6,25 @@
 //
 
 import Foundation
-
 import UIKit
 
 public class ThemeManager {
     public static let shared = ThemeManager()
-
-    private var theme: Theme
-
-    // Definir el nombre de la notificación
-    public static let themeDidChangeNotification = Notification.Name("themeDidChangeNotification")
-
-    private init() {
-        // Establecer un tema predeterminado
-        self.theme = Q4LTheme()
+    public static let themeDidChangeNotification = Notification.Name("ThemeDidChangeNotification")
+    
+    private var currentTheme: Theme = LightModeTheme() {
+        didSet {
+            NotificationCenter.default.post(name: ThemeManager.themeDidChangeNotification, object: nil)
+        }
     }
-
-    public func setTheme(_ newTheme: Theme) {
-        self.theme = newTheme
-        applyTheme()
-        
-        // Enviar notificación de cambio de tema
-        NotificationCenter.default.post(name: ThemeManager.themeDidChangeNotification, object: nil)
-    }
-
+    
     public func getTheme() -> Theme {
-        return theme
+        return currentTheme
     }
-
-    private func applyTheme() {
-        // Aquí puedes agregar lógica para aplicar el tema a toda la aplicación.
-        // Por ejemplo, cambiar el aspecto global de la barra de navegación o controles.
+    
+    public func setTheme(_ theme: Theme) {
+        currentTheme = theme
     }
+    
+    private init() {}
 }
